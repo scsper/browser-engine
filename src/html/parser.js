a195c491cbef46739b4fe4fc42a8b14b64ed3efa
Scott Sperling <scsper@gmail.com>
2016-12-27 17:57:00 -0800

    Move common parser logic to base class.

--- src/html/parser.js
+++ src/html/parser.js
@@ -1,5 +1,8 @@
 import {createElement, createText} from './dom';
 import invariant from '../invariant';
+import Parser from '../parser';
+
+const ALPHANUMERIC = /[a-zA-Z0-9]/;
 
 /**
  * Simple HTML parser that allows the following syntax:
@@ -10,69 +13,9 @@ import invariant from '../invariant';
  *
  * This is just a toy parser, so there is no real error handling, and we don't handle a lot of functionality.
  */
-
-const ALPHANUMERIC = /[a-zA-Z0-9]/;
-
-// @flow
-class Parser {
-  position: number;
-  input: string;
-
-  constructor(input = '') {
-    this.position = 0;
-    this.input = input;
-  }
-
-  /**
-   * Returns the current character in the input without advancing the position pointer.
-   */
-  peek(): string {
-    return this.input[this.position];
-  }
-
-  /**
-   * Returns TRUE if all input is consumed.
-   */
-  eof(): boolean {
-    return this.position >= this.input.length;
-  }
-
-  /**
-   * Answers the question "Do the next characters start with the given string?"
-   */
-  startsWith(s: string): boolean {
-    return this.input.startsWith(s, this.position);
-  }
-
-  /**
-   * Return the current character and advance the pointer to the next character.
-   */
-  consume(): string {
-    // order of operations is my friend here.
-    return this.input[this.position++];
-  }
-
-  /**
-   * Consumes characters that meet a given condition and returns them as a string.
-   * The argument is a function that takes a single character and returns a boolean.
-   */
-
-  // TODO: figure out the proper way to define a function as parameter in flow.
-  consumeWhile(test: any): string {
-    let consumedCharacters = '';
-
-    while (!this.eof() && test(this.peek())) {
-      consumedCharacters += this.consume();
-    }
-
-    return consumedCharacters;
-  }
-
-  /**
-   * Consumes all whitespace starting at the current position
-   */
-  consumeWhitespace(): void {
-    this.consumeWhile(c => !Boolean(c.trim().length));
+class DOMParser extends Parser {
+  constructor(input: string) {
+    super(input);
   }
 
   /**
@@ -198,7 +141,7 @@ class Parser {
  * Parse an HTML document and return the root element.
  */
 export function parse(source : String) : Node {
-  const parser = new Parser(source);
+  const parser = new DOMParser(source);
 
   return parser.parseNodes();
 }
---
+++ src/parser/index.js
@@ -0,0 +1,62 @@
+// @flow
+export default class Parser {
+  position: number;
+  input: string;
+
+  constructor(input = '') {
+    this.position = 0;
+    this.input = input;
+  }
+
+  /**
+   * Returns the current character in the input without advancing the position pointer.
+   */
+  peek(): string {
+    return this.input[this.position];
+  }
+
+  /**
+   * Returns TRUE if all input is consumed.
+   */
+  eof(): boolean {
+    return this.position >= this.input.length;
+  }
+
+  /**
+   * Answers the question "Do the next characters start with the given string?"
+   */
+  startsWith(s: string): boolean {
+    return this.input.startsWith(s, this.position);
+  }
+
+  /**
+   * Return the current character and advance the pointer to the next character.
+   */
+  consume(): string {
+    // order of operations is my friend here.
+    return this.input[this.position++];
+  }
+
+  /**
+   * Consumes characters that meet a given condition and returns them as a string.
+   * The argument is a function that takes a single character and returns a boolean.
+   */
+
+  // TODO: figure out the proper way to define a function as parameter in flow.
+  consumeWhile(test: any): string {
+    let consumedCharacters = '';
+
+    while (!this.eof() && test(this.peek())) {
+      consumedCharacters += this.consume();
+    }
+
+    return consumedCharacters;
+  }
+
+  /**
+   * Consumes all whitespace starting at the current position
+   */
+  consumeWhitespace(): void {
+    this.consumeWhile(c => !Boolean(c.trim().length));
+  }
+}
